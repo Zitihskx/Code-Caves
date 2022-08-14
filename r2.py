dfe27cc7848f90b56a9c65fd07f3631bb16c5d59
Zitihskx <33897365+Zitihskx@users.noreply.github.com>
2022-08-14 16:52:13 -0500

    Add files via upload

--- r2.py
+++ r2.py
@@ -1,33 +1,34 @@
-import argparse
-import json
-import logging
-import mmap
-import os
-import random
-import shutil
-import signal
-import threading
-import time
-from json import encoder
 import pefile
-import r2pipe
-import binascii
+import os 
+import mmap
+import sys
+import pandas as pd
+import time
 
-# #binary = "putty.exe"
-# binary = "0B7FEFAF5C8F3A320DC08EC32BD5955F0B3B2E35034C8B2AD879AE6BDC2CC0BC"
-# pe = pefile.PE(binary, fast_load=True)
-# if not pe.is_exe():
-#     pe.close()
-#     raise NotPE()
-# elif pe.OPTIONAL_HEADER.DATA_DIRECTORY[14].VirtualAddress != 0 or \
-#     pe.OPTIONAL_HEADER.DATA_DIRECTORY[14].Size != 0:
-#     pe.close()
-#     raise NotPE()
-# with open(binary,'rb') as f:
-#     hex_content = f.read().encode('hex')
+#binary = "0B7FEFAF5C8F3A320DC08EC32BD5955F0B3B2E35034C8B2AD879AE6BDC2CC0BC"
+start_time = time.time()
+directory = '/home/kshitiz/Desktop/Gradient Attack/data/train'
+#directory = '/home/kshitiz/Desktop/Gradient Attack/data/valid'
+#directory = '/home/kshitiz/Desktop/Code caves/Tes_malware'
 
-# print(hex_content)
+df = pd.DataFrame(columns = ['binary', 'size', 'count', 'per'])
+print(df)
 
-a = ['1','2', '3', '4', '5', '6', '7', '8']
+for files in os.scandir(directory):
+    if files.is_file():
+        try:
+            pe = pefile.PE(files, fast_load=True)
+        except:
+            continue
+        size = os.path.getsize(files)
+        count = 0
+        for each in pe.__data__:
+            if each == b'\x00':
+                count+=1
+        empty_per = count*100/size
 
-print(a[0:4])
+        data = {'binary': files, 'size' : size,'count' : count, 'per' : empty_per}
+        data_df = pd.DataFrame(data, index=[0])
+        df = pd.concat([df, data_df], ignore_index=True, sort=False)
+df.to_excel("Random1.xlsx")
+print(time.time()-start_time)
